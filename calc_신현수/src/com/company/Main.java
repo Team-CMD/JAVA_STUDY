c081c037478235d601009ae31d06ae6ad71e4394
sinbak <42922428+sinbak@users.noreply.github.com>
2021-11-24 11:52:58 +0900

    My testcode

---
+++ calc_신현수/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final int A = sc.nextInt();
+        final int B = sc.nextInt();
+
+        if(A>B)
+            System.out.println(">");
+        else if(A<B)
+            System.out.println("<");
+        else
+            System.out.println("==");
+    }
+
+}
